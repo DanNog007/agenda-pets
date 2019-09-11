d8b8cffb7a17a4b0a2e540aec4257aac557a4199
Daniel Magalhães <magalhaesdaniel2009@gmail.com>
2019-09-11 11:00:11 -0300

    Criação da estrutura inicial para os testes

---
+++ src/test/java/dn/soft/agendapets/GraphQlTest.java
@@ -0,0 +1,32 @@
+package dn.soft.agendapets;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringRunner;
+
+//import com.graphql.spring.boot.test.*;
+
+
+@RunWith(SpringRunner.class)
+//@GraphQLTest
+public class GraphQlTest {
+	
+	//@Autowired
+	//private GraphQLTestTemplate graphTemplate;
+	
+	@Test
+	public void test() throws IOException {
+		
+		//GraphQLResponse result = graphTemplate.postForResource("clientes.graphql");
+		//assertTrue(result.isOk());
+		
+		//TODO Implemetar testes futuramente
+		assertTrue(true);
+	}
+
+}
